47bb9984c07d884873c0c27a358af0a64ce645b0
Angelhome <amoreyra732@gmail.com>
2025-02-20 11:37:54 +0100

    cambios

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
 function showSurprise() {
-    document.getElementById('surprise').classList.toggle('hidden');
+    // document.getElementById('surprise').classList.toggle('hidden');
+    let surprise = document.getElementById('surprise');
+            surprise.classList.toggle('hidden');
+            surprise.classList.toggle('visible');
 }
